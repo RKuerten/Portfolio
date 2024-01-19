674d612dae6c35b3d085344168570d850ddc7b69
Rodrigo Kuerten <rodkue@gmail.com>
2024-01-19 11:20:54 -0300

    Fixed mobile behavior

--- assets/js/hoverChangeDescription-en.js
+++ assets/js/hoverChangeDescription-en.js
@@ -1,11 +1,18 @@
 export function hoverChangeDescription(nameCard, text) {
-  var changeDescription = document.querySelector(".changeDescription");
+  let changeDescription = document.querySelector(".changeDescription");
+  const isMobile = /iPhone|iPad|iPod|BlackBerry|Opera Mini|Android/i.test(
+    navigator.userAgent
+  );
 
   document.querySelector(nameCard).addEventListener("mouseover", () => {
     changeDescription.innerHTML = text;
   });
 
   document.querySelector(nameCard).addEventListener("mouseout", () => {
-    changeDescription.innerHTML = `On the right are some of the tools I'm proficient in! Hover your mouse cursor over the card to see each one!`;
+    if (isMobile) {
+      changeDescription.innerHTML = `Below are some of the tools I'm proficient in! Click on the cards to see each one!`;
+    } else {
+      changeDescription.innerHTML = `On the right are some of the tools I'm proficient in! Hover your mouse cursor over the card to see each one!`;
+    }
   });
 }
--- assets/js/hoverChangeDescription.js
+++ assets/js/hoverChangeDescription.js
@@ -1,12 +1,20 @@
 export function hoverChangeDescription(nameCard, text) {
-  var changeDescription = document.querySelector(".changeDescription");
+  let changeDescription = document.querySelector(".changeDescription");
+  const isMobile = /iPhone|iPad|iPod|BlackBerry|Opera Mini|Android/i.test(
+    navigator.userAgent
+  );
 
   document.querySelector(nameCard).addEventListener("mouseover", () => {
     changeDescription.innerHTML = text;
   });
 
   document.querySelector(nameCard).addEventListener("mouseout", () => {
-    changeDescription.innerHTML = `À direita estão algumas das ferramentas no qual eu tenho
+    if (isMobile) {
+      changeDescription.innerHTML = `Abaixo estão algumas das ferramentas no qual eu tenho
+      proficiência! Clique nos cards para conhecer cada um!`;
+    } else {
+      changeDescription.innerHTML = `À direita estão algumas das ferramentas no qual eu tenho
       proficiência! Passe o cursor do mouse no card para conhecer cada uma!`;
+    }
   });
 }
